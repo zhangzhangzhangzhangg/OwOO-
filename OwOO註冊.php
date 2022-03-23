ffef9b48227d23e93808b6ef7fbde35abd12589b
zhangzhangzhangzhangg <100786704+zhangzhangzhangzhangg@users.noreply.github.com>
2022-03-23 16:04:13 +0800

    Add files via upload

---
+++ OwOO註冊.php
@@ -0,0 +1,25 @@
+<head>
+<meta name="keywords" content="OwOO 註冊帳號,OwOO,OwO" />
+
+<title> OwOO註冊資訊 </title>
+<!--註解~~
+-->
+
+</head>
+<body bgcolor=#d8b2d3>
+<center><h1>OwOO</h1>
+<h2>註冊</h2>
+<FONT COLOR="white"></FONT>
+<h3><b>歡迎~~</b></h3>
+<form action="OwOO帳號rinfo.php" method="get" style="width: 80%;">
+<body>
+<HR><h2><b>基本資料</b></h2>
+Name:<input type ="text" name="uname" placeholder="姓名" required>
+<br/>Email:<input type ="email" name = "umail" placeholder="email" required>
+<br/>telephone:<input type ="telephone" name = "utele" placeholder="電話號碼" required>
+<br/>address:<textarea name="uadd" rows="1" cols="30" placeholder="地址" required></textarea>
+<br/>ID:<input type ="ID" name = "uid" placeholder="身分證字號" required>
+<br/>sex:<input type ="radio" name = "usex" value="1" />male<input type = "radio" name ="usex" value ="2"/>female<br/>
+<input type="hidden" name="type" value="Member"/></br>
+<input type="submit" value="註冊使用者">
+Thanks!
